5cea7c92e2e3eb5cdee3543a62cfae030f793d1c
Uyohoini Blankson <uyohoiniblankson11@gmail.com>
2022-03-12 08:46:58 +0100

    program Prints all numbers of base 10, starting from 0 with putchar only ( twice)

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Desc - program Print all numbers of base 10, starting from 0.
+ * You are not allowed to use any variable of type char.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` twice.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for  (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+
+	return (0);
+}
