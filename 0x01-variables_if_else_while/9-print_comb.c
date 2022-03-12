c7bf5cb7625f58b40427da8acbd9bdd9fb0eeada
Uyohoini Blankson <uyohoiniblankson11@gmail.com>
2022-03-12 10:22:31 +0100

    program that prints all possible combinations of single-digit numbers.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 /**
  * main - Entry Point
- * Desc - program that prints all possible combinations of single-digit numbers.
+ * Desc - program  prints all possible combinations of single-digit numbers.
  * * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i = 0;
 
-	while (i <10)
+	while (i < 10)
 	{
 		putchar(i + '0');
 		if (i < 9)
 		{
-			putchar(44); // , character
-			putchar(32); // space character
+			putchar(44); /* , character */
+			putchar(32); /* space character  */
 		}
 		i++;
 	}
