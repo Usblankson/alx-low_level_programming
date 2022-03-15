b00a29003aca7c1b1cc38b6d6ee449ff32cbfb15
Uyohoini Blankson <uyohoiniblankson11@gmail.com>
2022-03-15 14:07:55 +0100

    print putchar

---
+++ 0x02_functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry main
+ * desc - Program prints _putchar
+ *Return: 0
+ */
+int main(void)
+{
+	char s[] = "_putchar";
+
+	printf("%s", s);
+	putchar('\n');
+}
