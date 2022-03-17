22095ad9a486383c43ee69ae3eaadf25f42ade68
Uyohoini Blankson <uyohoiniblankson11@gmail.com>
2022-03-17 02:18:21 +0100

    print natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (n; n <= 98; n++)
+		for (; n <= 98; n++)
 		{
 			printf("%d", n);
 
@@ -25,7 +25,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (n; n >= 98; n--)
+		for (; n >= 98; n--)
 		{
 			printf("%d", n);
 
