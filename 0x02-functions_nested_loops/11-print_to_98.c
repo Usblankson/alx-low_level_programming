c7701fa5c82f2830f9986094718535ed867206c3
Uyohoini Blankson <uyohoiniblankson11@gmail.com>
2022-03-17 01:56:34 +0100

    print natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,7 +5,7 @@
  * @n: A input integer
  * Description: function that prints all natural numbers
  * from n to 98, followed by a new line.
- * Return: Nothing
+ * Return: N to 98
  */
 void print_to_98(int n)
 {
