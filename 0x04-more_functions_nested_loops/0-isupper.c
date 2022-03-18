7fb7bba7a1f8ed21f633c1c934d24d38b546b4ad
Uyohoini Blankson <uyohoiniblankson11@gmail.com>
2022-03-18 13:24:32 +0100

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
- *
  *_isupper - function that checks for uppercase characters
  *@c: int type number
  *Return: 1 if uppercase, else 0
@@ -9,9 +8,7 @@ int _isupper(int c)
 {
 
 	if ((c >= 'A') && (c <= 'Z'))
-
-	return (1);
+		return (1);
 	else
-
-	return (0);
+		return (0);
 }
