dffc9f42517fee234cf84b9a23fa5e760c550678
Uyohoini Blankson <uyohoiniblankson11@gmail.com>
2022-03-31 13:02:00 +0100

    function that copies a string.

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,17 +2,15 @@
 
 /**
  * _strncpy - two words
- * @dest : pointer to char params
- * @src : pointer to char params
- * @n : int params
+ * @dest : pointer to char param
+ * @src : pointer to char param
+ * @n : int param
  * Return: *dest
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	
 	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
 		dest[i] = src[i];
